25a17bb54d97e55dbe73a1b439019524ec582a2d
Tanner Damron <tanner.mdamron@yahoo.com>
2019-04-05 15:00:56 -0700

    add ability to add forums to firebase through admin component, working on adding forums to firebase through off topic component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserForum } from '../models/user-forum.model';
+import { UserForumsService } from '../user-forums.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [UserForumsService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adminForumService: UserForumsService) { }
 
   ngOnInit() {
   }
 
+  submitForum(title: string, subject: string, body: string) {
+    const currentTime = new Date();
+    const date = (currentTime.toString()).substr(0, 15);
+    const newForum = new UserForum(title, subject, body, date);
+    this.adminForumService.addAdminForum(newForum);
+  }
 }
--- src/app/user-forums.service.ts
+++ src/app/user-forums.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
 import { UserForum } from './models/user-forum.model';
 import { USERFORUMS, USERGAMEFORUMS, OFFTOPICFORUMS, STUDIOFORUMS } from './mock-userForums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class UserForumsService {
+  adminForums: FirebaseListObservable<any[]>;
+  offTopicForums: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.adminForums = database.list('adminForums');
+  }
+
+  getOffTopicForums() {
+    return this.offTopicForums;
+  }
+
+  addOffTopicForum(newForum: UserForum) {
+    this.offTopicForums.push(newForum);
+  }
+
+  getUserOffTopicForumBySubject(userGameForumSubject: string) {
+    for (let i = 0; i <= OFFTOPICFORUMS.length - 1; i++) {
+      if (OFFTOPICFORUMS[i].subject === userGameForumSubject) {
+        return OFFTOPICFORUMS[i];
+      }
+    }
+  }
+
+  getAdminForums() {
+    return this.adminForums;
+  }
+
+  addAdminForum(newForum: UserForum) {
+    this.adminForums.push(newForum);
+  }
 
   getUserForums() {
     return USERFORUMS;
@@ -27,22 +56,6 @@ export class UserForumsService {
     for (let i = 0; i <= USERGAMEFORUMS.length - 1; i++) {
       if (USERGAMEFORUMS[i].subject === userGameForumSubject) {
         return USERGAMEFORUMS[i];
-      }
-    }
-  }
-
-  getOffTopicForums() {
-    return OFFTOPICFORUMS;
-  }
-
-  getFirebaseOffTopicForums() {
-    return this.firebaseOffTopicForums;
-  }
-
-  getUserOffTopicForumBySubject(userGameForumSubject: string) {
-    for (let i = 0; i <= OFFTOPICFORUMS.length - 1; i++) {
-      if (OFFTOPICFORUMS[i].subject === userGameForumSubject) {
-        return OFFTOPICFORUMS[i];
       }
     }
   }
