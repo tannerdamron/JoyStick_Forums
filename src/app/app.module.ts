6adc8fad1036dc6eac4891e38bd42f2f41c04e63
Tanner Damron <tanner.mdamron@yahoo.com>
2019-04-05 09:31:03 -0700

    put both navbars into one navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
 
-
 import { AppComponent } from './app.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { TopnavComponent } from './topnav/topnav.component';
 import { UserForumComponent } from './user-forum/user-forum.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { StudiosForumsComponent } from './studios-forums/studios-forums.component';
 import { GamesForumsComponent } from './games-forums/games-forums.component';
 import { ForumDetailsComponent } from './forum-details/forum-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent,
-    TopnavComponent,
     UserForumComponent,
     HomePageComponent,
     StudiosForumsComponent,
     GamesForumsComponent,
-    ForumDetailsComponent
+    ForumDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+  }
+
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+  }
+}
