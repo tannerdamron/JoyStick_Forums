a7159ff44f68b3278477ef5335c030812fc32e65
Tanner Damron <tanner.mdamron@yahoo.com>
2019-04-12 12:56:10 -0700

    update readme

--- src/app/api-keys.ts
+++
@@ -1,7 +0,0 @@
-export const masterFirebaseConfig = {
-  apiKey: "AIzaSyAl1as-etvCJFu7XORfSRBG2Wl9qYBWqXM",
-  authDomain: "joystick-forums.firebaseapp.com",
-  databaseURL: "https://joystick-forums.firebaseio.com",
-  storageBucket: "joystick-forums.appspot.com",
-  messagingSenderId: "257443643600"
-};
